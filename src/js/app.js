93ff1c4d247975de370dd64af022bb3401452465
RagnarDanneskyold <martiniden148@yandex.ru>
2021-03-07 13:27:25 +0300

    extract sass at separate css file,also  add watch script

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,4 @@
 import './testModule.js';
 import './testModule2.js';
+import './oldScripts.js';
+import '/src/sass/app.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
     entry: './src/js/app.js',
@@ -7,8 +8,15 @@ module.exports = {
       path: path.resolve(__dirname, 'public/js'),
       filename: 'bundle.js'
     },
+    devtool:'eval-source-map',
     plugins: [
-      new CleanWebpackPlugin()
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: "[name].css",
+        chunkFilename: "[id].css",
+      }),
     ],
     module: {
       rules: [
@@ -17,6 +25,32 @@ module.exports = {
           exclude: /node_modules/,
           use: ['babel-loader'],
         },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [ 
+              {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                publicPath: './public/css',
+              },
+            },
+            {
+              loader: "css-loader",
+              options: {
+                sourceMap: true,
+              },
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        },
       ]
-    }
+    },
+    watchOptions: {
+      ignored: /node_modules/,
+    },
 }
